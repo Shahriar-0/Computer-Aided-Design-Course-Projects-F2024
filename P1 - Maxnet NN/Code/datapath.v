564d4efd8cd48c9e95c8b8ad7f5012cfd47c9a67
Shahriar-0 <attarshahriar@gmail.com>
2023-11-17 00:47:17 +0330

    names

--- P1 - Maxnet NN/Code/datapath.v
+++ P1 - Maxnet NN/Code/datapath.v
@@ -4,6 +4,8 @@ module DP(input clk, rst, ld1, ld2, ld3, ld4,
           output Co1, Co2);
 
     wire [1:0] row, column;
+    wire [31:0] R1Out, R2Out, R3Out, R4Out;
+    wire [31:0] W1Out, W2Out, W3Out, W4Out;
 
     Counter2bit rowCounter(.rst(rst), .clk(clk), .init(initCounter1), .en(enCounter1), .out(row), .Co(Co1));
     Counter2bit columnCounter(.rst(rst), .clk(clk), .init(initCounter2), .en(enCounter2), .out(column), .Co(Co2)); 
