e432a131810d4f3a73a3c7b94a820cadfe6c63b6
Shahriar-0 <attarshahriar@gmail.com>
2023-11-17 00:32:47 +0330

    Fix module name and formatting in Matrix.v

--- P1 - Maxnet NN/Code/Matrix.v
+++ P1 - Maxnet NN/Code/Matrix.v
@@ -1,7 +1,7 @@
-module matrix (input [1:0] row,
+module Matrix (input [1:0] row,
                column,
                output reg [31:0] Out);
 
     assign Out = (row == column)? 32'b00111110010011001100110011001101 : 32'b0;
-    
+
 endmodule
