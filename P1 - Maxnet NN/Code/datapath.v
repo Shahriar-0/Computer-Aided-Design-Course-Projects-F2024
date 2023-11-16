c480b57fae453bdc933a5d8503a27d599e6f8bf3
Shahriar-0 <attarshahriar@gmail.com>
2023-11-17 00:26:09 +0330

    Refactor datapath module to improve readability and organization

--- P1 - Maxnet NN/Code/datapath.v
+++ P1 - Maxnet NN/Code/datapath.v
@@ -1,21 +1,12 @@
-module TopModule (
-    input clock, reset, writeEn,
-    input [31:0] address, write_data, num1, num2, num3, num4, weight1, weight2, weight3, weight4,
-    output [31:0]result
-);
-    wire [31:0] data_out, relu_out,read_data;
-    wire [31:0]  weight1, weight2, weight3, weight4,
-    output [31:0]result
-    wire s1, s2, s3, s4;
-    wire [1:0] findmax;
-    DataMemory mem1 (.clock(clock), .writeEn(writeEn), .reset(reset), .address(address), .write_data(data_out), .read_data(read_data));
+module DP(input clk, rst, ld1, ld2, ld3, ld4, 
+          initCounter1, initCounter2, enCounter1, enCounter2, 
+          input [31:0] regInput,
+          output Co1, Co2);
 
-    Register reg1 (.clock(clock), .reset(reset), .data_in(write_data), .data_out(data_out));
+    wire [1:0] row, column;
+
+    Counter2bit rowCounter(.rst(rst), .clk(clk), .init(initCounter1), .en(enCounter1), .out(row), .Co(Co1));
+    Counter2bit columnCounter(.rst(rst), .clk(clk), .init(initCounter2), .en(enCounter2), .out(column), .Co(Co2)); 
     
     
-    DataMemory mem1 (.clock(clock), .writeEn(writeEn), .reset(reset), .address(address), .write_data(data_out), .read_data(read_data));
-    relu relu1 (.x(num1), .y(relu_out));
-    findmaxout max1 (.s1(s1), .s2(s2), .s3(s3), .s4(s4), .findmax(findmax));
-    ProcessingUnit proc1 (.num1(num1), .num2(num2), .num3(num3), .num4(num4), .weight1(weight1), .weight2(weight2), .weight3(weight3), .weight4(weight4), .clk(clock), .result(result));
-
 endmodule
