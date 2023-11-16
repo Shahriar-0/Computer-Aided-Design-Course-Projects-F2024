bcbcdd359d148f68c4ca366168385cad4870880f
Shahriar-0 <attarshahriar@gmail.com>
2023-11-17 00:34:09 +0330

    fix number

--- P1 - Maxnet NN/Code/Matrix.v
+++ P1 - Maxnet NN/Code/Matrix.v
@@ -2,6 +2,6 @@ module Matrix (input [1:0] row,
                column,
                output reg [31:0] Out);
 
-    assign Out = (row == column)? 32'b00111110010011001100110011001101 : 32'b0;
+    assign Out = (row == column)? 32'b1_01111100_10011001100110011001101 : 32'b0;
 
 endmodule
