b2e701b7417765b021ad97094a67d8fc07da435d
Shahriar-0 <attarshahriar@gmail.com>
2023-11-19 01:59:50 +0330

    new register

--- P1 - Maxnet NN/Code/Register.v
+++ P1 - Maxnet NN/Code/Register.v
@@ -1,17 +1,16 @@
 module Register (
     input clk,
     input rst,
-    input load_enable,
-    input [31:0] data_in,
-    output reg [31:0] data_out
+    input ld,
+    input [31:0] in,
+    output reg [31:0] out
 );
-    initial data_out = 32'b0;
     
-    always @(posedge clk, posedge rst) begin
+    always @(posedge clk or posedge rst) begin
         if (rst)
-            data_out <= 0;
-        else if (load_enable)
-            data_out <= data_in;
+            out <= 0;
+        else if (ld)
+            out <= in;
     end
     
 endmodule
