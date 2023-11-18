32ef90368e6ee67ae36626d93b5dc4b2c5f5ee9b
Shahriar-0 <attarshahriar@gmail.com>
2023-11-19 01:54:14 +0330

    heb

--- P1 - Maxnet NN/Code/FloatingAdder.v
+++ P1 - Maxnet NN/Code/FloatingAdder.v
@@ -1,32 +1,49 @@
-module Multiplier #(parameter N = 32)
-                   (input [N-1:0] a,
-                    b,
-                    output reg [N-1:0] result);
+module Adder #(parameter N = 32)
+              (input [N-1:0] a,
+               b,
+               output reg [N-1:0] result);
     
-    reg [47:0] tempMantis;
-    reg [23:0] aMantis, bMantis;
+    reg [23:0] aMantis, bMantis, tmpMantis;
     reg [22:0] resultMantis;
-    reg [7:0] aExp, bExp, tmpExp, resultExp;
-    reg aSign, bSign, resultSign;
+    reg [7:0] aExp, bExp, diffExp, resultExp;
+    reg aSign, bSign, resultSign, carry, comp;
+    wire MSb;
     
     always @(*) begin
-        aMantis = {1'b1,a[22:0]};
-        aExp    = a[30:23];
-        aSign   = a[31];
+        comp = (a[30:23] > b[30:23])? 1'b1 :
+        (a[30:23] == b[30:23] && a[22:0] > b[22:0]) ? 1'b1 : 1'b0;
         
-        bMantis = {1'b1,b[22:0]};
-        bExp    = b[30:23];
-        bSign   = b[31];
+        aMantis = comp ? {1'b1,a[22:0]} : {1'b1,b[22:0]};
+        aExp    = comp ? a[30:23] : b[30:23];
         
-        tmpExp     = aExp + bExp - 127;
-        tempMantis = aMantis * bMantis;
+        aSign = comp ? a[31] : b[31];
         
-        resultMantis = tempMantis[47] ? tempMantis[46:24] : tempMantis[45:23];
-        resultExp    = tempMantis[47] ? tmpExp + 1'b1 : tmpExp;
+        bMantis = comp ? {1'b1,b[22:0]} : {1'b1,a[22:0]};
+        bExp    = comp ? b[30:23] : a[30:23];
         
-        resultSign = aSign ^ bSign;
+        bSign = comp ? b[31] : a[31];
         
-        result = (a == 32'b0 || b == 32'b0) ? 0 : {resultSign, resultExp, resultMantis};
+        diffExp = aExp - bExp;
+        
+        bMantis = (bMantis >> diffExp);
+        
+        {carry, tmpMantis} = (aSign  ~^ bSign) ? (aMantis + bMantis) : (aMantis - bMantis);
+        
+        resultExp = aExp;
+        
+        if (carry) begin
+            tmpMantis = tmpMantis>>1;
+            resultExp = resultExp+1'b1;
+        end
+        else begin
+            while(!tmpMantis[23]) begin
+                tmpMantis = tmpMantis<<1;
+                resultExp = resultExp-1'b1;
+            end
+        end
+        
+        resultSign   = aSign;
+        resultMantis = tmpMantis[22:0];
+        result       = {resultSign, resultExp, resultMantis};
     end
-    
 endmodule
--- P1 - Maxnet NN/Code/FloatingMultiplier.v
+++ P1 - Maxnet NN/Code/FloatingMultiplier.v
@@ -1,32 +1,32 @@
-`timescale 1ns / 1ps
-module FloatingMultiplication #(parameter XLEN = 32)
-                               (input [XLEN-1:0]A,
-                                input [XLEN-1:0]B,
-                                input clk,
-                                output reg [XLEN-1:0] result);
+module Multiplier #(parameter N = 32)
+                   (input [N-1:0] a,
+                    b,
+                    output reg [N-1:0] result);
     
-    reg [23:0] A_Mantissa,B_Mantissa;
-    reg [22:0] Mantissa;
-    reg [47:0] Temp_Mantissa;
-    reg [7:0] A_Exponent, B_Exponent, Temp_Exponent, diff_Exponent, Exponent;
-    reg A_sign, B_sign, Sign;
-    reg [32:0] Temp;
-    reg [6:0] exp_adjust;
+    reg [47:0] tempMantis;
+    reg [23:0] aMantis, bMantis;
+    reg [22:0] resultMantis;
+    reg [7:0] aExp, bExp, tmpExp, resultExp;
+    reg aSign, bSign, resultSign;
     
-    always@(*) begin
-        A_Mantissa = {1'b1,A[22:0]};
-        A_Exponent = A[30:23];
-        A_sign     = A[31];
+    always @(*) begin
+        aMantis = {1'b1,a[22:0]};
+        aExp    = a[30:23];
+        aSign   = a[31];
         
-        B_Mantissa = {1'b1,B[22:0]};
-        B_Exponent = B[30:23];
-        B_sign     = B[31];
+        bMantis = {1'b1,b[22:0]};
+        bExp    = b[30:23];
+        bSign   = b[31];
         
-        Temp_Exponent = A_Exponent + B_Exponent-127;
-        Temp_Mantissa = A_Mantissa * B_Mantissa;
-        Mantissa      = Temp_Mantissa[47] ? Temp_Mantissa[46:24] : Temp_Mantissa[45:23];
-        Exponent      = Temp_Mantissa[47] ? Temp_Exponent + 1'b1 : Temp_Exponent;
-        Sign          = A_sign ^ B_sign;
-        result        = {Sign, Exponent, Mantissa};
+        tmpExp     = aExp + bExp - 127;
+        tempMantis = aMantis * bMantis;
+        
+        resultMantis = tempMantis[47] ? tempMantis[46:24] : tempMantis[45:23];
+        resultExp    = tempMantis[47] ? tmpExp + 1'b1 : tmpExp;
+        
+        resultSign = aSign ^ bSign;
+        
+        result = (a == 32'b0 || b == 32'b0) ? 0 : {resultSign, resultExp, resultMantis};
     end
+    
 endmodule
