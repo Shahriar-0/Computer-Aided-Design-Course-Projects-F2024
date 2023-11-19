d407b4dced951d110a182f8f0e3048ff489bc0f2
Shahriar-0 <attarshahriar@gmail.com>
2023-11-19 07:24:34 +0330

    be over

--- P1 - Maxnet NN/Code/DP.v
+++ P1 - Maxnet NN/Code/DP.v
@@ -11,8 +11,8 @@ module DP(input clk,
     wire [31:0] relu1Out, relu2Out, relu3Out, relu4Out;
     wire [31:0] mux1Out, mux2Out, mux3Out, mux4Out;
     wire [31:0] x1Out, x2Out, x3Out, x4Out;
-    wire [31:0] val1Out, val2Out, val3Out, val4Out;
-    wire val1OutOr, val2OutOr, val3OutOr, val4OutOr;
+    wire [31:0] val1, val2, val3, val4;
+    wire val1Or, val2Or, val3Or, val4Or;
     wire [31:0] w11, w12, w13, w14;
     wire [31:0] w21, w22, w23, w24;
     wire [31:0] w31, w32, w33, w34;
@@ -32,7 +32,7 @@ module DP(input clk,
     Register x2(.clk(clk), .rst(rst), .ld(ldX), .in(readData2), .out(x2Out));
     Register x3(.clk(clk), .rst(rst), .ld(ldX), .in(readData3), .out(x3Out));
     Register x4(.clk(clk), .rst(rst), .ld(ldX), .in(readData4), .out(x4Out));
-    Decoder decoder(.in1(val1OutOr), .in2(val2OutOr), .in3(val3OutOr), .in4(val4OutOr), .out(decoderOut));
+    Decoder decoder(.in1(val1Or), .in2(val2Or), .in3(val3Or), .in4(val4Or), .out(decoderOut));
     Mux4to1 outputMux(.a(x1Out), .b(x2Out), .c(x3Out), .d(x4Out), .sel(decoderOut), .out(maxnumber));
     
     // this part for recording value of x1, x2, x3, x4 as the model trains
@@ -41,10 +41,10 @@ module DP(input clk,
     Mux2to1 mux3(.a(readData3), .b(relu3Out), .sel(selTmp), .out(mux3Out));
     Mux2to1 mux4(.a(readData4), .b(relu4Out), .sel(selTmp), .out(mux4Out));
     
-    Register tmp1(.clk(clk), .rst(rst), .ld(ldTmp), .in(mux1Out), .out(val1Out));
-    Register tmp2(.clk(clk), .rst(rst), .ld(ldTmp), .in(mux2Out), .out(val2Out));
-    Register tmp3(.clk(clk), .rst(rst), .ld(ldTmp), .in(mux3Out), .out(val3Out));
-    Register tmp4(.clk(clk), .rst(rst), .ld(ldTmp), .in(mux4Out), .out(val4Out));
+    Register tmp1(.clk(clk), .rst(rst), .ld(ldTmp), .in(mux1Out), .out(val1));
+    Register tmp2(.clk(clk), .rst(rst), .ld(ldTmp), .in(mux2Out), .out(val2));
+    Register tmp3(.clk(clk), .rst(rst), .ld(ldTmp), .in(mux3Out), .out(val3));
+    Register tmp4(.clk(clk), .rst(rst), .ld(ldTmp), .in(mux4Out), .out(val4));
     
     // this part is for pu and relu
     Matrix matrix(
@@ -54,34 +54,34 @@ module DP(input clk,
         .W12(w41), .W13(w42), .W14(w43), .W15(w44)
     );
 
-    assign val1OutOr = |val1Out;
-    assign val2OutOr = |val2Out;
-    assign val3OutOr = |val3Out;
-    assign val4OutOr = |val4Out;
+    assign val1Or = |val1;
+    assign val2Or = |val2;
+    assign val3Or = |val3;
+    assign val4Or = |val4;
     
-    Check checker(.in1(val1OutOr), .in2(val2OutOr), .in3(val3OutOr), .in4(val4OutOr), .out(done));
+    Check checker(.in1(val1Or), .in2(val2Or), .in3(val3Or), .in4(val4Or), .out(done));
     
     ProcessingUnit p1(
         .clk(clk), .rst(rst), .result(PU1Out),
-        .num1(val1Out), .num2(val2Out), .num3(val3Out), .num4(val4Out),
+        .num1(val1), .num2(val2), .num3(val3), .num4(val4),
         .weight1(w11), .weight2(w12), .weight3(w13), .weight4(w14)
     );
 
     ProcessingUnit p2(
         .clk(clk), .rst(rst), .result(PU2Out),
-        .num1(val1Out), .num2(val2Out), .num3(val3Out), .num4(val4Out),
+        .num1(val1), .num2(val2), .num3(val3), .num4(val4),
         .weight1(w21), .weight2(w22), .weight3(w23), .weight4(w24)
     );
 
     ProcessingUnit p3(
         .clk(clk), .rst(rst), .result(PU3Out),
-        .num1(val1Out), .num2(val2Out), .num3(val3Out), .num4(val4Out),
+        .num1(val1), .num2(val2), .num3(val3), .num4(val4),
         .weight1(w31), .weight2(w32), .weight3(w33), .weight4(w34)
     );
 
     ProcessingUnit p4(
         .clk(clk), .rst(rst), .result(PU4Out),
-        .num1(val1Out), .num2(val2Out), .num3(val3Out), .num4(val4Out),
+        .num1(val1), .num2(val2), .num3(val3), .num4(val4),
         .weight1(w41), .weight2(w42), .weight3(w43), .weight4(w44)
     );
         
--- P1 - Maxnet NN/Code/TopModule.v
+++ P1 - Maxnet NN/Code/TopModule.v
@@ -1,8 +1,10 @@
 module TopModule(input clk,
                  rst,
                  start,
+                 output done,
                  output [31:0] maxnumber);
-    wire ldX, ldTmp, selTmp, done;
-    DP dp(.clk(clk), .rst(rst), .maxnumber(maxnumber), .done(done), .ldX(ldX), .ldTmp(ldTmp), .selTmp(selTmp));
-    CU cu(.clk(clk), .rst(rst), .start(start), .done(done), .ldX(ldX), .ldTmp(ldTmp), .selTmp(selTmp));
+    wire ldX, ldTmp, selTmp, doneInternal;
+    DP dp(.clk(clk), .rst(rst), .maxnumber(maxnumber), .done(doneInternal), .ldX(ldX), .ldTmp(ldTmp), .selTmp(selTmp));
+    CU cu(.clk(clk), .rst(rst), .start(start), .done(doneInternal), .ldX(ldX), .ldTmp(ldTmp), .selTmp(selTmp));
+    assign done = doneInternal;
 endmodule
