ca25274d4668b9bff4459e821f85434921acc2e8
Shahriar-0 <attarshahriar@gmail.com>
2023-11-19 04:39:18 +0330

    new matrix

--- P1 - Maxnet NN/Code/CU.v
+++ P1 - Maxnet NN/Code/CU.v
@@ -1,16 +1,24 @@
-`define IDLE         6'd0
-`define LD1          6'd1
-`define LD2          6'd2
-`define LD3          6'd3
-`define LD4          6'd4
-`define MUL          6'd5
-`define SUM1         6'd6
-`define SUM2         6'd7
-`define ACTIVE_FUNC  6'd8
-`define CHECK        6'd9
-`define STORE        6'd10 
+`define IDLE 6'd0
+`define INIT 6'd4
+`define MUL  6'd5
+`define ADD  6'd6
+`define LD   6'd7
 
-module CU(clk, rst, start, done, flag, ld1, ld2, ld3, ld4,mux1_sel, ldm1, ldm2, ldm3, ldm4,address,l1,l2,l3);
+module CU(clk,
+          rst,
+          start,
+          done,
+          flag,
+          ld1,
+          mux1_sel,
+          ldm1,
+          ldm2,
+          ldm3,
+          ldm4,
+          address,
+          l1,
+          l2,
+          l3);
     
     input clk, rst, start, flag;
     output reg ld1, ld2, ld3, ld4, ldm1, ldm2, ldm3, ldm4, mux1_sel,l1,l2,l3;
@@ -45,16 +53,16 @@ module CU(clk, rst, start, done, flag, ld1, ld2, ld3, ld4,mux1_sel, ldm1, ldm2, 
     always @(ps) begin
         {ld1, ld2, ld3, ld4,ldm1, ldm2, ldm3, ldm4, mux1_sel, done,address,l1,l2,l3} = 15'b0;
         case (ps)
-            `IDLE        :{ld1, ld2, ld3, ld4,ldm1, ldm2, ldm3, ldm4, mux1_sel, done} = 15'b0;
-            `LD1         :{ld1, ldm1,mux1_sel,address} = 5'b11000;
-            `LD2         :{ld2, ldm2,mux1_sel,address} = 5'b11001 ;
-            `LD3         :{ld3, ldm3,mux1_sel,address} = 5'b11010  ;
-            `LD4         :{ld4, ldm4,mux1_sel,address} = 5'b11011 ;
-            `MUL         :{l1,l2,l3} = 3'b100;
-            `SUM1        :{l1,l2,l3} = 3'b010;
-            `SUM2        :{mux1_sel,l1,l2,l3} = 4'b1001;
-            `CHECK       :{ld1, ld2, ld3, ld4,mux1_sel} = 5'b11111;
-            `STORE       :{done}=1'b1 ;
+            `IDLE        :{ld1, ld2, ld3, ld4,ldm1, ldm2, ldm3, ldm4, mux1_sel, done}                  = 15'b0;
+            `LD1         :{ld1, ldm1,mux1_sel,address}                                                 = 5'b11000;
+            `LD2         :{ld2, ldm2,mux1_sel,address}                                                 = 5'b11001 ;
+            `LD3         :{ld3, ldm3,mux1_sel,address}                                                 = 5'b11010  ;
+            `LD4         :{ld4, ldm4,mux1_sel,address}                                                 = 5'b11011 ;
+            `MUL         :{l1,l2,l3}                                                                   = 3'b100;
+            `SUM1        :{l1,l2,l3}                                                                   = 3'b010;
+            `SUM2        :{mux1_sel,l1,l2,l3}                                                          = 4'b1001;
+            `CHECK       :{ld1, ld2, ld3, ld4,mux1_sel}                                                = 5'b11111;
+            `STORE       :{done}                                                                       = 1'b1 ;
             default      :{ld1, ld2, ld3, ld4,ldm1, ldm2, ldm3, ldm4, mux1_sel, done,address,l1,l2,l3} = 15'b0;
         endcase
     end
--- P1 - Maxnet NN/Code/Matrix.v
+++ P1 - Maxnet NN/Code/Matrix.v
@@ -1,21 +1,42 @@
-module Matrix (input [1:0] row,
-               column,
-               output reg [31:0] Out);
-    reg [31:0] matrix [0:3][0:3];
-    integer i;
-    integer j;
-    
-    initial begin
-        for (i = 0; i < 4; i = i + 1) begin
-            for(j = 0; j < 4; j = j + 1)begin
-                if (i == j)begin
-                    matrix[i][j] = 32'b00111110010011001100110011001101;
-                end
-                else begin
-                    matrix[i][j] = 32'b0;
-                end
+module Matrix (output reg [31:0] W0,
+               W1,
+               W2,
+               W3,
+               W4,
+               W5,
+               W6,
+               W7,
+               W8,
+               W9,
+               W10,
+               W11,
+               W12,
+               W13,
+               W14,
+               W15);
+
+integer i, j;
+
+reg [31:0] buffer [0:15];
+
+initial begin
+    for (i = 0; i < = 3; i = i + 1) begin
+        for (j = 0; j < = 3; j = j + 1) begin
+            if (i == j) begin
+                // 1.0 in IEEE 754 format
+                buffer[i*4+j] = 32'b00111111100000000000000000000000;
+            end
+            else begin
+                // -0.2 in IEEE 754 format
+                buffer[i*4+j] = 32'b10111110010011001100110011001101;
             end
         end
     end
-    assign Out = matrix[row][column];
+end
+
+assign {W0,  W1,  W2,  W3}  = {buffer[0],  buffer[1],  buffer[2],  buffer[3]};
+assign {W4,  W5,  W6,  W7}  = {buffer[4],  buffer[5],  buffer[6],  buffer[7]};
+assign {W8,  W9,  W10, W11} = {buffer[8],  buffer[9],  buffer[10], buffer[11]};
+assign {W12, W13, W14, W15} = {buffer[12], buffer[13], buffer[14], buffer[15]};
+
 endmodule
