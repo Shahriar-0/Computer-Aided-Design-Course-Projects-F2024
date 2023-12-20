47987961adcf1cd58db017b189eb13a9245f3478
Shahriar-0 <attarshahriar@gmail.com>
2023-12-20 05:01:22 +0330

    update relu

--- P2 - Logic Design and FPGA synthesis/trunk/src/hdl/ReLU.v
+++ P2 - Logic Design and FPGA synthesis/trunk/src/hdl/ReLU.v
@@ -1,9 +1,12 @@
 module ReLU #(parameter XLEN = 32) (
     input wire [XLEN - 1:0] in,
+
     output wire [XLEN - 1:0] out
 );
     
-    assign out = in[XLEN - 1]? 32'd0 : in;
+    // assign out = in[XLEN - 1]? 32'd0 : in;
     
+    MUX2 #(XLEN) mux(in, 0, in[XLEN - 1], out);
+
 endmodule
     
