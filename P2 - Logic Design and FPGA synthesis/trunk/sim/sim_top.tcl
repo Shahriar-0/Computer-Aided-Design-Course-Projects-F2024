cd3201b441f3b8c068ec11a5d8f61558d2b223a5
Shahriar-0 <attarshahriar@gmail.com>
2023-12-20 06:25:12 +0330

    clean

--- P2 - Logic Design and FPGA synthesis/trunk/sim/sim_top.tcl
+++ P2 - Logic Design and FPGA synthesis/trunk/sim/sim_top.tcl
@@ -12,21 +12,58 @@
 	# set run_time			"-all"
 
 #============================   verilog files  ==================================
-
-	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/Check.v
-	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/CU.v
-	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/DataMemory.v
-	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/Decoder.v
-	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/DP.v
-	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/FloatingAddition.v
-	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/FloatingMultiplication.v
-	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/Matrix.v
-	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/Mux2to1.v
-	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/Mux4to1.v
-	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/ProcessingUnit.v
-	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/Register.v
-	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/ReLU.v
-	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/TopModule.v		
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/Adder.v             
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/AdderCascade.v      
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/ADD_TB.v            
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/AND2.v              
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/AND3.v              
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/AND4.v              
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/AND4NOT3.v          
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/AND4NOT4.v          
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/BIT_MULT.v          
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/BUF.v               
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/Check.v             
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/CU.v                
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/CU2.v               
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/DataMemory.v        
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/Decoder.v           
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/DP.v                
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/MULT.v              
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/MULT_TB.v           
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/MUX2.v              
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/MUX2REG.v           
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/MUX2REGSingle.v     
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/MUX2Single.v        
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/Mux2to1.v           
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/MUX4.v              
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/MUX4REG3.v          
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/MUX4REGSingle.v     
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/Mux4to1.v           
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/MUX8.v              
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/MUX8REG.v           
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/MUX8REG_TB.v        
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/NOT.v               
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/OR3.v               
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/OR4.v               
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/OR5.v               
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/ProcessingUnit.v    
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/PU1.v               
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/PU2.v               
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/PU3.v               
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/PU4.v               
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/REG.v               
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/Register.v          
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/ReLU.v              
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/ThreeBitComparator.v
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/TopModule.v         
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/XOR2.v              
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/XOR3.v              
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/Zeroetend2to3.v     
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/ZeroExtend.v        
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/ZeroExtend2to3_TB.v 
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/__ACT_C1.v          
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/__ACT_C2.v          
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/__ACT_S2.v
 	vlog 	+acc -incr -source  +incdir+$inc_path +define+SIM 	./tb/$TB.v
 	onerror {break}
 
