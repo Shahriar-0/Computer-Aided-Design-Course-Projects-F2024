1c33b0e9e88ee35ac850fa6a8de603403dc15dee
Shahriar-0 <attarshahriar@gmail.com>
2023-12-20 03:49:12 +0330

    added PUs

---
+++ P2 - Logic Design and FPGA synthesis/trunk/src/hdl/PU4.v
@@ -0,0 +1,17 @@
+module PU4 #(parameter XLEN = 5) (
+    input clk, rst,
+    input [XLEN - 1:0] num1, num2, num3, num4,
+
+    output [XLEN - 1:0] result
+);
+
+    wire [XLEN - 1:0] mult1, mult2, mult3, mult4, 
+                      R1Out, R2Out, R3Out, R4Out, 
+                      add1, add2, add3;
+
+    assign mult1 = {3'b100, num1[XLEN - 2: XLEN - 3]};
+    assign mult2 = {3'b100, num2[XLEN - 2: XLEN - 3]};
+    assign mult3 = {3'b100, num3[XLEN - 2: XLEN - 3]};
+    assign mult4 = num4;
+
+endmodule
