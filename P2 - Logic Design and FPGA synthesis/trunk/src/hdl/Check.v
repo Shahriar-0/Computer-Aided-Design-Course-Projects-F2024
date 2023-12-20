97656044d18920751e58e72243d9ebc5efdb9910
Shahriar-0 <attarshahriar@gmail.com>
2023-12-20 05:01:43 +0330

    update check

--- P2 - Logic Design and FPGA synthesis/trunk/src/hdl/Check.v
+++ P2 - Logic Design and FPGA synthesis/trunk/src/hdl/Check.v
@@ -7,12 +7,12 @@ module Check(
     
     wire w1, w2, w3, w4, w5;
     
-    assign w1  =  in1 & ~in2 & ~in3 & ~in4;
-    assign w2  = ~in1 &  in2 & ~in3 & ~in4;
-    assign w3  = ~in1 & ~in2 &  in3 & ~in4;
-    assign w4  = ~in1 & ~in2 & ~in3 &  in4;
-    assign w5  = ~in1 & ~in2 & ~in3 & ~in4;
+    AND4NOT3 and4not3_0(in1, in2, in3, in4, w1);
+    AND4NOT3 and4not3_1(in2, in1, in3, in4, w2);
+    AND4NOT3 and4not3_2(in3, in2, in1, in4, w3);
+    AND4NOT3 and4not3_3(in4, in2, in3, in1, w4);
+    AND4NOT4 and4not4_0(w1, w2, w3, w4, w5);
 
-    assign out = w1 | w2 | w3 | w4 | w5;
+    OR5 or5_0(w1, w2, w3, w4, w5, out);
     
 endmodule
