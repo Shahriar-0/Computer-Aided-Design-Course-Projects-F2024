e895793c84bdaa5b12b4c302c004e2a52030ff2b
Shahriar-0 <attarshahriar@gmail.com>
2023-12-20 06:14:53 +0330

    update controller

--- P2 - Logic Design and FPGA synthesis/trunk/src/hdl/CU.v
+++ P2 - Logic Design and FPGA synthesis/trunk/src/hdl/CU.v
@@ -1,8 +1,20 @@
-`define IDLE 3'd0
-`define INIT 3'd1
-`define MUL  3'd2
-`define ADD  3'd3
-`define LD   3'd4
+`define IDLE 3'b000
+`define INIT 3'b001
+`define MUL  3'b010
+`define ADD  3'b011
+`define LD   3'b100
+
+// BIT 0
+//      is 1 in INIT and ADD state
+//      INIT => (IDLE & start) | (INIT & start)
+//      ADD  => MUL 
+// BIT 1
+//      is 1 in MUL and ADD state
+//      MUL  => (INIT & ~start) | (LD & ~done)
+//      ADD  => MUL 
+// BIT 2
+//      is 1 in LD state
+//      LD   => ADD
 
 module CU(
     input clk, rst, start, done,
@@ -12,16 +24,24 @@ module CU(
     
     reg [2:0] ns, ps;
     
-    always @(*) begin
-        case (ps)
-            `IDLE  : ns = (start)? `INIT: `IDLE;
-            `INIT  : ns = (start)? `INIT: `MUL;
-            `MUL   : ns = `ADD;
-            `ADD   : ns = `LD;
-            `LD    : ns = (done)? `IDLE: `MUL;
-            default: ns = `IDLE;
-        endcase
-    end
+    wire isIDLE, isINIT, isMUL, isADD, isLD,
+         IDLEandSTART, INITandSTART, 
+         INITandNOTSTART, LDandNOTDONE;
+        
+    assign isIDLE = (ps == `IDLE);
+    assign isINIT = (ps == `INIT);
+    assign isMUL  = (ps == `MUL);
+    assign isADD  = (ps == `ADD);
+    assign isLD   = (ps == `LD);
+
+    AND2 and2_0(isIDLE, start, IDLEandSTART);
+    AND2 and2_1(isINIT, start, INITandSTART);
+    AND2 and2_2(isINIT, ~start, INITandNOTSTART);
+    AND2 and2_3(isLD, ~done, LDandNOTDONE);
+
+    OR3 or3_0(IDLEandSTART, INITandSTART, isMUL, ns[0]);
+    OR3 or3_1(INITandNOTSTART, LDandNOTDONE, isMUL, ns[1]);
+    assign ns[2] = isADD;
     
     always @(ps) begin
         {ldX, ldTmp, selTmp} = 3'b0;
