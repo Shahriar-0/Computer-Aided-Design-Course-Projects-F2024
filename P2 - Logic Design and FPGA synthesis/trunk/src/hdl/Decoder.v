b4c43148accf30afd5fe1c9e54c1aa79ca6b7f3f
Shahriar-0 <attarshahriar@gmail.com>
2023-12-20 03:57:13 +0330

    optimized decoder

--- P2 - Logic Design and FPGA synthesis/trunk/src/hdl/Decoder.v
+++ P2 - Logic Design and FPGA synthesis/trunk/src/hdl/Decoder.v
@@ -1,19 +1,10 @@
 module Decoder(
     input in1, in2, in3, in4,
+
     output reg [1:0] out
 );
-
-    always @(*) begin
-        if (in1)
-            out = 2'b00;
-        else if (in2)
-            out = 2'b01;
-        else if (in3)
-            out = 2'b10;
-        else if (in4)
-            out = 2'b11;
-        else
-            out = 2'b00;
-    end
     
+    AND2 and1(in4, in2, out[0]);
+    AND2 and2(in4, in3, out[1]);
+
 endmodule
